c71fdb8a2dbb6662904a165546ee23682c0c4662
Deepa Rajendran <deeparajendran54@gmail.com>
2024-04-27 12:55:25 +0530

    To-Do List

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
 
+//select button
+
+var add=document.getElementById("addbtn");
+var listing=document.getElementById("listing");
+var listItems=document.querySelector(".list-items");
+// var div=document.querySelector(".list")
+
+function addList(){
+    var div=document.createElement("div");
+    div.setAttribute("class","list");
+    var text=listing.value;
+    if(text!=""){
+    div.innerHTML=`<p>${text}</p>
+    <button id="deletebtn" onclick="deleteList(event)">Delete</button>`;
+    listItems.append(div);
+    }else{
+        window.alert("Please add a list");
+    }
+    listing.value="";
+}
+
+function deleteList(event){
+    event.target.parentElement.remove();
+}
